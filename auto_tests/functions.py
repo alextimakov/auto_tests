8a4b2e0917ad6d24b6bde279f859220962d0665e
alextimakov <alextimakoff94@gmail.com>
2019-10-30 12:46:09 +0300

    added update_many to mongodb - test version; fixed several bson.errors; added docstrings to key functions

--- auto_tests/functions.py
+++ auto_tests/functions.py
@@ -7,6 +7,7 @@ import json
 from time import sleep, time, strftime, gmtime
 from datetime import datetime
 from pandas.io.json import json_normalize
+from bson import errors
 
 
 def aggregate_dash(db, collection, pipeline):
@@ -95,14 +96,14 @@ def read_poll_job(poll_job):
     """
 
     try:
-        text = poll_job.json()['data'][0]
+        text = poll_job.json()['data']
     except ValueError:
         text = poll_job.text
     return text
 
 
 def collect_data(db, collection_dashboards, collection_collections, pipeline, black_list):
-    # Сбор dashboard_id, metrics_id с prod
+    # Сбор списка дашбордов и метрик с MongoDB_prod
     df_prod = mongo_request(db, collection_dashboards, pipeline, 'metric_id')
 
     # Собираем все модули
@@ -111,7 +112,7 @@ def collect_data(db, collection_dashboards, collection_collections, pipeline, bl
     cursor_collections['name'] = json_normalize(cursor_collections['name'])
     collections = cursor_collections.loc[~cursor_collections['name'].isin(black_list)]['_id'].tolist()
 
-    # собираем _id релевантных дашбордов, имя - для доппроверки # change to read_mongo
+    # собираем _id релевантных дашбордов, имя - для доппроверки
     needs_dashboards = read_mongo(db, collection_dashboards, query={
         "$and": [{"deleted": False}, {"general.published": True}, {"general.collectionId": {"$in": collections}}]},
                                             output={'_id': 1, 'general.name.ru': 1})
@@ -137,11 +138,30 @@ def run_sso_session(sso, user, password, logger, add_cookies=True, custom_header
 
 def auto_test(data_frame, sso, api, website, user, password, prefix, logger, db_qa, dashboards,
               add_cookies, custom_headers, headers):
+    """
+    Key function to start and poll metrics on selected instance via API
+
+    :param (pandas.core.frame.DataFrame) data_frame: existing metrics from prod
+    :param (str) sso: url to sso
+    :param (str) api: url to api
+    :param (str) website: dns
+    :param user: username
+    :param password: password
+    :param (str) prefix: prefix like 'prod' or 'qa'
+    :param logger: logger
+    :param (pandas.core.frame.DataFrame) db_qa: db for getting variables from qa
+    :param (str) dashboards: collection with dashboard
+    :param (bool) add_cookies: if True then add '_currentUser' and 'roles' cookies
+    :param (bool) custom_headers: if True then add custom headers
+    :param (dict) headers: dictionary with custom headers
+    :return:
+    """
     session = run_sso_session(sso, user, password, logger, add_cookies, custom_headers, headers)
     result = pd.DataFrame()
     for i in range(0, data_frame.shape[0]):
         dashboard = data_frame.loc[i, 'dashboard']
         metric_id = data_frame.loc[i, 'metric_id']
+
         # сбор тела запроса метрики
         logger.info("job {} initiated by {}".format(int(i), user))
         pipeline_var = [{"$group": {"_id": "$metrics.{}.variables".format(metric_id)}},
@@ -154,12 +174,13 @@ def auto_test(data_frame, sso, api, website, user, password, prefix, logger, db_
                                              "metricId": "{}".format(metric_id),
                                              "variables": aggregation['variables']}}}
 
-
+        # отправка запроса
         try:
             start_job = session.post(api+'startJob', json=body, timeout=60)
         except requests.exceptions.Timeout:
-            logger.info("timeout occured at metric {}".format(metric_id))
-            result = write_results(result, i, dashboard, metric_id, 'Timeout', 0, 400, website, prefix)
+            start_job = 'start_job_timeout'
+            logger.info('{} at metric {}'.format(start_job, metric_id))
+            result = write_results(result, i, dashboard, metric_id, str(start_job), 0, 400, website, prefix)
             break
         logger.info("metric {} run with code {} by {}".format(metric_id, int(start_job.status_code), user))
         if int(start_job.status_code) in [403, 500]:
@@ -187,8 +208,8 @@ def auto_test(data_frame, sso, api, website, user, password, prefix, logger, db_
                 try:
                     poll_job = session.get(api+'pollJob/'+job, timeout=60)
                 except requests.exceptions.ReadTimeout:
-                    poll_job = 'Timeout'
-                    logger.info("metric {} read timeout".format(metric_id))
+                    poll_job = 'poll_job_timeout'
+                    logger.info('{} at metric {}'.format(poll_job, metric_id))
                     break
                 logger.info("metric {} polled with code {}".format(metric_id, int(poll_job.status_code)))
                 if poll_job.status_code != 204:
@@ -196,8 +217,8 @@ def auto_test(data_frame, sso, api, website, user, password, prefix, logger, db_
                         int(time()-sleeper)))
                     break
                 sleep(1)
-            if poll_job == 'Timeout':
-                text = 'Timeout'
+            if poll_job == 'poll_job_timeout':
+                text = poll_job
             else:
                 text = read_poll_job(poll_job)
             result = write_results(result, i, dashboard, metric_id, str(text), int(time()-sleeper),
@@ -210,19 +231,41 @@ def insert_test_results(db, collection, df_list):
     db[collection].insert_many(df_list.T.to_dict().values(), ordered=False)
 
 
-def update_many(db, collection, condition, cond_value, df):
-    for column in df.columns:
-        db[collection].update_many(filter={condition: df[cond_value]},
-                                   update={'$set': {column: df[column].values}})
+def update_many(db, collection, condition, df, merger):
+    # NOT FINISHED !!!
+    unique_metrics = df[condition].unique().tolist()
+    unique_columns = [column for column in df.columns.tolist() if column not in merger]
+    for instance in unique_metrics:
+        for column in df.loc[df[condition] == instance][unique_columns]:
+            try:
+                db[collection].update_many({condition: instance},
+                                           {'$set': {column: str(df.loc[df[condition] == instance][column].values[0])}})
+            except errors.InvalidDocument:
+               pass
 
-def run_multiple_tests(df_data, df_processor, merger, db_put, collection_put, dfs):
-    # run test + merge with post-processor + merge with exceptions + save to excel + write to db
+
+def run_multiple_tests(df_data, df_processor, merger, db_put, collection_put, dfs, save_to_excel=False):
+    """
+    Run test -> merge with post-processor -> [merge with exceptions] -> save to excel | write to mongo
+
+    :param (pandas.core.frame.DataFrame) df_data: existing metrics from prod
+    :param (pandas.core.frame.DataFrame) df_processor: existing metrics with post-processor
+    :param (str) merger: field to merge on
+    :param db_put: db to put results of tests into
+    :param collection_put: collections to put results of tests into
+    :param (dicts) dfs: configs for dfs to run tests on
+    :param (bool) save_to_excel: if true then save to xlsx files
+    :return: None
+    """
     for df in dfs:
         try:
             df_result = auto_test(df_data, **df)
             df_result = df_result.merge(df_processor, how='left', on=merger)
-        except ValueError or KeyError:
+            df_result = df_result.rename(columns={'post_processor': 'post_processor_{}'.format(df['prefix'])})
+            # merge with exceptions
+        except ValueError or KeyError:  # set up relevant errors
             df_result = pd.DataFrame()
-        df_result.to_excel('auto_tests_{prefix}_{Time}.xlsx'.format(prefix=df['prefix'],
-            Time=strftime("%Y-%m-%d", gmtime())), index=False)
-        # update_many(db_put, collection_put, 'metric_id', 'metric_id', df_result)  # not finished!
+        if save_to_excel:
+            df_result.to_excel('auto_tests_{prefix}_{Time}.xlsx'.format(prefix=df['prefix'],
+                Time=strftime("%Y-%m-%d", gmtime())), index=False)
+        update_many(db_put, collection_put, 'metric_id', df_result, merger)
--- auto_tests/main.py
+++ auto_tests/main.py
@@ -9,18 +9,16 @@ import logging.config as logging_config
 import getpass
 
 # TODO: логирование - прописать разграничение уровней ошибок (в logger.config)
-# TODO: переписать append'ы результата
-# TODO: написать тест для запуска по меньшему числу метрик и убрать df.head()
+# TODO: переписать append'ы результата на что-то более эффективное
+# TODO: написать тесты для запуска по меньшему числу метрик и убрать все df.head()
 # TODO: настроить сборку через tox.ini
-# TODO: разобраться с Makefile и раскаткой через Travis.CI
 # TODO: написать алёрты на мониторинг выполнения скрипта
-# TODO: переписать def main() корректно
-# TODO: актуализировать setup.py и сделать автоматический semver
-# TODO: логирование - обмен результатами логирования, оценить возможность развернуть ELK и складывать логи туда
+# TODO: переписать def main() по-человечески
+# TODO: актуализировать setup.py и сделать автоматический semver (возможно ли?)
+# TODO: логирование - обмен результатами логирования, оценить возможность складывать логи в БД
 # TODO: проверять наличие прописанных exceptions в теле метрик с пост-обработчиком
 # TODO: перевести список коллекций в словарь и дёргать их по ключам
-# TODO: переписать единый insert на несколько поступательных update
-# TODO: перенести compare results в метрику
+# TODO: перенести compare results в метрику в Analytics
 
 # set up logging
 log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logger.config')
@@ -31,20 +29,18 @@ logger = logging.getLogger(__name__)
 def main():
     df = functions.collect_data(config.db_prod, config.collection_dashboards, config.collection_collections,
                                 scripts.pipeline_dash, config.black_list)
-    functions.insert_test_results(config.db_prod, config.collection_auto_tests, df)
     df_processor = functions.mongo_request(config.db_prod, config.collection_dashboards,
                                            scripts.pipeline_processor)
-    # df = df.head(5)
 
-    # credentials configuration
+    # credentials
     login: str = getpass.getuser() + config.mail
     password: str = getpass.getpass()
 
-    user_cluster = config.user_cluster
-    password_cluster = config.password_cluster
+    user_cluster: str = config.user_cluster
+    password_cluster: str = config.password_cluster
     headers_cluster = config.headers_cluster
 
-    # dfs configuration
+    # dfs to test
     df_prod = {'sso': config.sso_prod, 'api': config.api_prod, 'website': config.site_prod, 'user': login,
         'password': password, 'prefix': 'prod', 'logger': logger, 'db_qa': config.db_qa,
         'dashboards': config.collection_dashboards, 'add_cookies': True, 'custom_headers': False, 'headers': {}}
@@ -57,9 +53,16 @@ def main():
         'headers': headers_cluster}
     dfs = [df_prod, df_qa, df_cluster]
 
-    # run auto tests
-    functions.run_multiple_tests(df, df_processor, config.merger, config.db_prod, config.collection_auto_tests, dfs)
-    return 'Done'
+    # limit for testing
+    # df = df.head(3)
+    # df_processor = df_processor.head(3)
+
+    # check for existing metric_id before updating?
+    functions.insert_test_results(config.db_prod, config.collection_auto_tests, df)
+
+    # run multiple tests
+    functions.run_multiple_tests(df, df_processor, config.merger, config.db_prod, config.collection_auto_tests, dfs,
+                                 save_to_excel=False)
 
 
 if __name__ == '__main__':
